915f7c17081107b3c04f0b10455018e2086df8ce
Afam Ifeanyi(Chrys) M <affam_ifeanyi@live.com>
2023-01-18 13:05:46 +0200

    Update Gemfile adding redis and devise. Build a stream using turbo_stream and made the application realtime

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
+# Quote class
 class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+  # after_create_commit lambda {
+  #                       broadcast_prepend_to 'quotes', partial: 'quotes/quote', locals: { quote: self },
+  #                                                      target: 'quotes'
+  #                     }
+
+  # Using ruby's synctactic sugar
+  # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
+  # after_update_commit -> { broadcast_replace_later_to 'quotes' }
+  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
+
+  # Using ruby's synctactic sugar
+  broadcasts_to ->(_quote) { 'quotes' }, inserts_by: :prepend
 end
